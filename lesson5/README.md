c6f2fb73071815943dccb9f0a4b09150484fbaa4
Madhav Sathe <msathe-tech@gmail.com>
2019-12-23 16:05:58 -0500

    Fix lesson5 code, make it work with custom Topic names

--- lesson5/README.md
+++ lesson5/README.md
@@ -1,8 +1,11 @@
 # Join KStream and KTable using BiFunction
+This application demonstrates ability to join KStream and KTable and generate an output KStream.
+The application has following components -
 
-ToDo - 
-Write code to generate load on userClickStream and userRegionTable.
-Try using CommandLineRunning and role.
+1. populateUserRegionMap - this loads USER_REGION topic with map of <User, Region>
+2. generateClickStream - this loads USER_CLICKS topic with <User, Clicks>
+3. streamTableBiFunction - this joins USER_CLICKS and USER_REGION and produces REGION_CLICKS with map of <Region, Clicks>
+4. checkRegionClicks - prints REGION_CLICKS stream 
 
 ## Start the application 
 App should start once the Kafka is running. 
@@ -20,19 +23,3 @@ Access the docker container for the broker.
 Use CLI to send messages on the topic. 
 
 `kafka-topics --zookeeper zookeeper:2181 --list`
-
-Start one producer console in one shell - 
-
-`kafka-console-producer --broker-list localhost:9092 --topic languageWordCount-in-0`
-
-Start first consumer console in second shell - 
-
-`kafka-console-consumer --bootstrap-server localhost:9092 --topic english`
-
-Start second consumer console in third shell - 
-
-`kafka-console-consumer --bootstrap-server localhost:9092 --topic french`
-
-Start third consumer in another shell - 
-
-`kafka-console-consumer --bootstrap-server localhost:9092 --topic hindi`
--- lesson5/src/main/java/com/example/demo/DemoApplication.java
+++ lesson5/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 import java.util.function.BiFunction;
-
+import java.util.function.Consumer;
+import java.util.function.Function;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -12,27 +15,29 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.ForeachAction;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
-
 	static final String BOOTSTRAP_SERVER = "localhost:9092";
-	static final String USER_CLICKS_TOPIC = "streamTableBiFunction-in-0";
+	static final String USER_CLICKS_TOPIC = "USER_CLICKS";
+	static final String USER_REGION_TOPIC = "USER_REGION";
 	static final String[] USERS = new String[] {"1", "2", "3", "4", "5", "6", "7", "8"};
 	static final String[] REGIONS = new String[] {"east", "west", "north", "south"};
-	static final UserRegsionMap[] USER_REGIONS = new UserRegsionMap[] {
+	static final List<UserRegsionMap> USER_REGION_LIST = Arrays.asList(new UserRegsionMap[] {
 			new UserRegsionMap(REGIONS[0], USERS[0]),
 			new UserRegsionMap(REGIONS[0], USERS[1]),
 			new UserRegsionMap(REGIONS[1], USERS[2]),
@@ -41,8 +46,7 @@ public class DemoApplication {
 			new UserRegsionMap(REGIONS[3], USERS[5]),
 			new UserRegsionMap(REGIONS[3], USERS[6]),
 			new UserRegsionMap(REGIONS[3], USERS[7]),
-	};
-
+	});
 	static final Long[] CLICKS = new Long[] {
 			(long) 10,
 			(long) 20,
@@ -55,11 +59,14 @@ public class DemoApplication {
 			(long) 30,
 			(long) 80,
 	};
+	static final Properties USER_CLICK_TOPIC_PROPS = getStreamsConfiguration("org.apache.kafka.common.serialization.StringSerializer",
+			"org.apache.kafka.common.serialization.LongSerializer");
+	static final Properties USER_REGION_TOPIC_PROPS = getStreamsConfiguration("org.apache.kafka.common.serialization.StringSerializer",
+			"org.apache.kafka.common.serialization.StringSerializer");
 
 	Random random = new Random();
 
 	static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -67,62 +74,78 @@ public class DemoApplication {
 
 	@Bean
 	public BiFunction<KStream<String, Long>, KTable<String, String>, KStream<String, Long>>  streamTableBiFunction() {
-		return (userClickStream, userRegionTable) -> (userClickStream
-			.leftJoin(userRegionTable,
-					(clicks, region) ->
-							new RegionWithClicks(region == null ? "UNKNOWN" : region, clicks),
-					Joined.with(Serdes.String(), Serdes.Long(), null))
-			.map((user, regionWithClicks) -> new KeyValue<>(regionWithClicks.getRegion(), regionWithClicks.getClicks()))
-			.groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
-			.reduce(Long::sum)
-			.toStream()
-		);
-	}
-
-	@Bean
-	@ConditionalOnProperty(name="role", havingValue = "generateClickStream", matchIfMissing = false)
-	public CommandLineRunner generateClickStream() {
-		return (args) -> {
-			Properties props = getStreamsConfiguration("generateClickStream", "org.apache.kafka.common.serialization.StringSerializer",
-					"org.apache.kafka.common.serialization.LongSerializer");
-
-			Producer<String, Long> producer = new KafkaProducer<String, Long>(props);
-
-			ProducerRecord<String, Long> userClick = new ProducerRecord<>(USER_CLICKS_TOPIC,
-					USERS[random.nextInt(8)], CLICKS[random.nextInt(10)]);
-
-			RecordMetadata metadata = producer.send(userClick).get();
-
-			log.info("partion = {} offset = {}", metadata.partition(), metadata.offset());
-
-			producer.flush();
-			producer.close();
-
-
+		return (userClickStream, userRegionTable) -> {
+			userClickStream.foreach(new ForeachAction<String, Long>() {
+				@Override
+				public void apply(String s, Long aLong) {
+					System.out.println("foo: " + s + aLong);
+				}
+			});
+			return (userClickStream
+					.leftJoin(userRegionTable,
+							(clicks, region) ->
+									new RegionWithClicks(region == null ? "UNKNOWN" : region, clicks),
+							Joined.with(Serdes.String(), Serdes.Long(), null))
+					.map((user, regionWithClicks) -> new KeyValue<>(regionWithClicks.getRegion(), regionWithClicks.getClicks()))
+					.groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+					.reduce(Long::sum)
+					.toStream()
+			);
 		};
 	}
 
-	static Properties getStreamsConfiguration(final String client_id,
-			final String keySerdeClass, final String valueSerdeClass ) {
+	@Bean
+	public Consumer<KStream<String, Long>> checkRegionClicks() {
+		return input -> input.foreach((region, clicks) ->
+				log.info("Region = {} clicks = {}", region, clicks));
+	}
+
+	@Scheduled(initialDelay = 1000, fixedRate = 5000)
+	public void generateClickStream() {
+		Producer<String, Long> producer = new KafkaProducer<String, Long>(USER_CLICK_TOPIC_PROPS);
+		ProducerRecord<String, Long> userClick = new ProducerRecord<>(USER_CLICKS_TOPIC,
+				USERS[random.nextInt(8)], CLICKS[random.nextInt(10)]);
+		try {
+			RecordMetadata metadata = producer.send(userClick).get();
+			log.info("Message sent to partion = {} offset = {}", metadata.partition(), metadata.offset());
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		producer.flush();
+		producer.close();
+	}
+
+	@Bean
+	public CommandLineRunner populateUserRegionMap() {
+		return (args) -> {
+			USER_REGION_LIST.forEach((userRegion) -> sendUserRegion(userRegion));
+		};
+	}
+
+	static void sendUserRegion(UserRegsionMap userRegion) {
+		Producer<String, String> producer = new KafkaProducer<String, String>(USER_REGION_TOPIC_PROPS);
+		ProducerRecord<String, String> userRegionToSend = new ProducerRecord<>(USER_REGION_TOPIC, userRegion.getUser(),
+				userRegion.getRegion());
+		try {
+			RecordMetadata metadata = producer.send(userRegionToSend).get();
+			log.info("UserRegion record added to partition = {} offset = {}", metadata.partition(), metadata.offset());
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		producer.flush();
+		producer.close();
+	}
+
+	static Properties getStreamsConfiguration(final String keySerdeClass, final String valueSerdeClass ) {
 		final Properties streamsConfiguration = new Properties();
-		// Give the Streams application a unique name.  The name must be unique in the Kafka cluster
-		// against which the application is run.
-		//streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "lesson5-streamTableBiFunction");
-		//streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG,  client_id);
-		// Where to find Kafka broker(s).
+
 		streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
 		// Specify default (de)serializers for record keys and for record values.
 		streamsConfiguration.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerdeClass);
 		streamsConfiguration.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerdeClass);
-		// Records should be flushed every 10 seconds. This is less than the default
-		// in order to keep this example interactive.
-		// streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
-		// For illustrative purposes we disable record caches.
-		// streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-		// Use a temporary directory for storing state, which will be automatically removed after the test.
-		//streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory().getAbsolutePath());
+
 		return streamsConfiguration;
 	}
-
-
 }
+
+
